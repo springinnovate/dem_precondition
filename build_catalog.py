34e459aedf1350dce4cd3463dfacd01e0a0449a0
Richard P Sharp Jr <richpsharp@gmail.com>
2025-03-14 16:20:27 -0700

    intermediate commit, working on wlaing that directory structure with 'Path'

--- build_catalog.py
+++ build_catalog.py
@@ -1,9 +1,13 @@
 # build_catalog.py
+import json
 import os
 import pystac
 import rasterio
 from shapely.geometry import box, mapping
 from pathlib import Path
+from ecoshard import geoprocessing
+
+from osgeo import gdal
 
 
 def create_catalog(root_dir, catalog_id, catalog_description):
@@ -12,48 +16,65 @@ def create_catalog(root_dir, catalog_id, catalog_description):
         description=catalog_description
     )
 
-    collection = pystac.Collection(
-        id=f'{catalog_id}-collection',
-        description="Pre-routed DEM slices aligned with HUC05s",
-        extent=pystac.Extent(
-            spatial=pystac.SpatialExtent([[-180, -90, 180, 90]]),
-            temporal=pystac.TemporalExtent([[None, None]])
+    indexing_vector = gdal.OpenEx('data/global_lev05.gpkg')
+    indexing_layer = indexing_vector.GetLayer()
+
+    prefix_list = [
+        ('filled_dem',
+         'filled',
+         'Depression filled dem.'),
+        ('flow_dir_mfd',
+         'flow_dir_mfd',
+         'geoprocessing.routing.flow_dir_mfd routed dem.')]
+
+    for collection_prefix, collection_id, collection_description in prefix_list:
+        collection = pystac.Collection(
+            id=f'{catalog_id}-{collection_prefix}',
+            description=collection_description,
+            extent=pystac.Extent(
+                spatial=pystac.SpatialExtent([[-180, -90, 180, 90]]),
+                temporal=pystac.TemporalExtent([[None, None]])
+            )
         )
-    )
-    catalog.add_child(collection)
+        catalog.add_child(collection)
 
-    root_path = Path(root_dir).resolve()
+        root_path = Path(root_dir).resolve()
 
-    for root, _, files in os.walk(root_path):
-        for fname in files:
-            if fname.lower().endswith((".tif", ".tiff")):
-                full_path = Path(root) / fname
-                with rasterio.open(full_path) as src:
-                    bounds = src.bounds
-                    geom = box(*bounds)
-                    bbox = [bounds.left, bounds.bottom, bounds.right, bounds.top]
+        for current_dir_path, _, file_list in os.walk(root_path):
+            for filename in file_list:
+                if (filename.endswith('.tif') and
+                        filename.startswith(collection_prefix)):
+                    raster_path = current_dir_path / filename
+                    raster_info = geoprocessing.get_raster_info(raster_path)
 
-                item = pystac.Item(
-                    id=full_path.stem,
-                    geometry=mapping(geom),
-                    bbox=bbox,
-                    datetime=None,
-                    properties={}
-                )
+                    raster_stem = Path(raster_path).stem
+                    fid = int(raster_stem.split('_'))[-1]
+                    feature = indexing_layer.GetFeature(fid)
+                    geom = feature.GetGeometryRef()
+                    geom_json_text = geom.ExportToJson()
+                    geojson_geom = json.loads(geom_json_text)
 
-                item.add_asset(
-                    key="data",
-                    asset=pystac.Asset(
-                        href=str(full_path),
-                        media_type=pystac.MediaType.COG
+                    item = pystac.Item(
+                        id=raster_stem,
+                        geometry=geojson_geom,
+                        bbox=raster_info['bounding_box'],
+                        datetime=None,
+                        properties={}
                     )
-                )
 
-                collection.add_item(item)
+                    item.add_asset(
+                        key="data",
+                        asset=pystac.Asset(
+                            href=raster_path,
+                            media_type=pystac.MediaType.GEOTIFF
+                        )
+                    )
 
-    catalog.normalize_and_save(
-        root_href="stac-catalog", 
-        catalog_type=pystac.CatalogType.SELF_CONTAINED)
+                    collection.add_item(item)
+
+        catalog.normalize_and_save(
+            root_href="stac-catalog",
+            catalog_type=pystac.CatalogType.SELF_CONTAINED)
 
 
 if __name__ == "__main__":
